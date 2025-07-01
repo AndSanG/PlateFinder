284a08cb53e7bb19778ab242d1e58ea0e76adf95
Andres Sanchez <and_sg@outlook.com>
2025-07-01 18:38:44 -0500

    Initial Commit

---
+++ PlateFinder/PlateFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlateFinderApp.swift
+//  PlateFinder
+//
+//  Created by Andres Sanchez on 01/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct PlateFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
